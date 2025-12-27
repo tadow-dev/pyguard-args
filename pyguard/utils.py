1dca18011ff03d6826ffd5531b9fb49c6227a9dd
mateuszjasinski <matij95@gmail.com>
2025-12-27 23:13:01 +0100

    chore: formatting

--- pyguard/utils.py
+++ pyguard/utils.py
@@ -8,15 +8,16 @@ def is_hint_optional(annotation: Any) -> bool:
     """
     origin = get_origin(annotation)
     args = get_args(annotation)
-    
+
     # Handle Union[X, None]
     if origin is Union and type(None) in args:
         return True
-    
+
     # Handle X | None syntax (Python 3.10+)
     if sys.version_info >= (3, 10):
         import types
+
         if isinstance(annotation, types.UnionType):
             return type(None) in get_args(annotation)
-    
+
     return False
